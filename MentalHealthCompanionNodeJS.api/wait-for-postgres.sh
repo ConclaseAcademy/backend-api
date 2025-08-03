b32676fa0b3388defe0fa14c28161c93e9bd17bb
Dreees <ayeniidris0@gmail.com>
2025-08-03 15:36:03 +0100

    Updating Dockerfile

---
+++ MentalHealthCompanionNodeJS.api/wait-for-postgres.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+host="$DB_HOST"
+shift
+
+until PGPASSWORD=$DB_PASSWORD psql -h "$host" -U "$DB_USER" -d "$DB_DATABASE" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 2
+done
+
+>&2 echo "Postgres is up - executing command"
+exec "$@"
